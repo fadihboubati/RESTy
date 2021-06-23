f1bce21df3e82046aa0ab7d99b682eea97c04d2b
fadiHB <eng.fadi.hb@gmail.com>
2021-06-23 12:51:51 +0300

    refactor PUT

--- src/API/api.js
+++ src/API/api.js
@@ -11,6 +11,13 @@ export const fetchData = async (myUrl, method, text) => {
         setting.body = text; // body data type must match "Content-Type" header
         setting.headers = {'Content-Type': 'application/json'};
         // setting.mode = "no-cors";
+        await fetch(url, setting);
+        let lastParamIndx = url.lastIndexOf('/') + 1;
+        let urlWithoutParam = url.slice(0, lastParamIndx);
+        url = urlWithoutParam;
+        setting = {};
+        console.log("url", url);
+        setting.method = "GET";
         res = await fetch(url, setting);
 
     } else if (method === "DELETE") {
@@ -37,21 +44,3 @@ export const fetchData = async (myUrl, method, text) => {
     
     return { "body": body, "headers": res.headers };
 };
-
-// async function postData(url = '', data = {}) {
-//     // Default options are marked with *
-//     const response = await fetch(url, {
-//       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//       mode: 'cors', // no-cors, *cors, same-origin
-//       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//       credentials: 'same-origin', // include, *same-origin, omit
-//       headers: {
-//         'Content-Type': 'application/json'
-//         // 'Content-Type': 'application/x-www-form-urlencoded',
-//       },
-//       redirect: 'follow', // manual, *follow, error
-//       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//       body: JSON.stringify(data) // body data type must match "Content-Type" header
-//     });
-//     return response.json(); // parses JSON response into native JavaScript objects
-//   }
